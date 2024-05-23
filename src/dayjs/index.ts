67228d7224e2f650212c64d4d75d54815512395b
Valerii Sidorenko <balepas@nebius.com>
2024-05-23 19:29:29 +0200

    fix(locale): add formats to en locale

--- src/dayjs/index.ts
+++ src/dayjs/index.ts
@@ -3,9 +3,13 @@ import customParseFormat from 'dayjs/plugin/customParseFormat';
 import updateLocale from 'dayjs/plugin/updateLocale';
 import utc from 'dayjs/plugin/utc';
 
+import {englishFormats} from '../constants';
+
 dayjs.extend(customParseFormat);
 dayjs.extend(utc);
 dayjs.extend(updateLocale);
+
+dayjs.Ls['en'].formats = englishFormats;
 
 export default dayjs;
 
